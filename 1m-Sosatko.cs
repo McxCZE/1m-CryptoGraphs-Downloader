87fe33815056d7a5bbe0f54dad45b2d0a020726b
McxCZE <milan.cihak@brouzdej.net>
2021-10-06 13:17:58 +0200

    Added Config.Json support

--- 1m-Sosatko.cs
+++ 1m-Sosatko.cs
@@ -13,70 +13,103 @@ namespace _1m_CryptoGraphs_Downloader
     {
         private static void Main(string[] args)
         {
+            //Definuj proměnné. 
+            string TradingPair = "";
+            string Exchange = "";
+            string StartDate = "";
+            string StopDate = "";
+            string JSON_ConfigFile = System.IO.File.ReadAllText("Config.json");
+            JSON_Configs JSON_Config = JsonConvert.DeserializeObject<JSON_Configs>(JSON_ConfigFile);
 
-            //UI
+            //JSON - Values
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Zadej prosím symbol (podporované zadání (např. Binance (ADAUSDT), např FTX (ADA-PERP)) : ");
+            Console.Write("Pokud chceš načíst nastavení v Config.JSON, zadej A, pokud ne, zadej N : ");
             Console.ForegroundColor = ConsoleColor.Green;
-            string TradingPair = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Vyber burzu (FTX/BINANCE): ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            string Exchange = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Zadej prosím počáteční datum (od kdy mam stáhnout data, formát např. 01.01.2021) : ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            string StartDate = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Zadej prosím koncové datum (do kdy mam stáhnout data, formát např. 01.02.2021) : ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            string StopDate = Console.ReadLine();
+            string JsonAsk = Console.ReadLine();
+
+            if (JsonAsk == "A")
+            {
+                int i = 0;
+                    while (JSON_Config.Pairs.Count > i)
+                    {
+                        TradingPair = JSON_Config.Pairs[i].TradingPair;
+                        Exchange = JSON_Config.Pairs[i].Exchange;
+                        StartDate = JSON_Config.Pairs[i].StartDate;
+                        StopDate = JSON_Config.Pairs[i].StopDate;
+                        i++;
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine();
+                        Console.WriteLine("Vybrané volby");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Symbol: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(TradingPair);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Burza: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(Exchange);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Od kdy: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(StartDate);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("Do kdy: ");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(StopDate);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine();
+                    }
 
 
+            } else
+            {
+                //UI
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Zadej prosím symbol (podporované zadání (např. Binance (ADAUSDT), např FTX (ADA-PERP)) : ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                TradingPair = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Vyber burzu (FTX/BINANCE): ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Exchange = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Zadej prosím počáteční datum (od kdy mam stáhnout data, formát např. 01.01.2021) : ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                StartDate = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Zadej prosím koncové datum (do kdy mam stáhnout data, formát např. 01.02.2021) : ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                StopDate = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+                Console.WriteLine("Vybrané volby");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Symbol: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(TradingPair);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Burza: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(Exchange);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Od kdy: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(StartDate);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Do kdy: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(StopDate);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+            }
 
             //Dummy Variables
             //string TradingPair = "ADAUPUSDT";
             //string Exchange = "BINANCE";
             //string StartDate = "10.09.2021";
             //string StopDate = "22.09.2021";
-
-
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine();
-            Console.WriteLine("Vybrané volby");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Symbol: ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(TradingPair);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Burza: ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(Exchange);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Od kdy: ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(StartDate);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("Do kdy: ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(StopDate);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine();
-
-
-            // Definuj časový rozsah, jak v sekundách, tak v milisekundách.
-            TimeSpan StartT = DateTime.Parse(StartDate) - DateTime.UnixEpoch;
-            int StartT_seconds = (int)StartT.TotalSeconds;
-            double StartT_miliseconds = (double)StartT.TotalMilliseconds;
-            TimeSpan StopT = DateTime.Parse(StopDate) - DateTime.UnixEpoch;
-            int StopT_seconds = (int)StopT.TotalSeconds;
-            double StopT_miliseconds = (double)StopT.TotalMilliseconds;
-            //
-
-            //
-            string EnvPath = $@"%USERPROFILE%\Documents\1m-Grafy\{TradingPair}_{StartDate}_{StopDate}.csv";
-            string SavePath = (Environment.ExpandEnvironmentVariables(EnvPath)).Replace("/", "");
-            //
 
             // Kontrola Prerekvizit.
             string EnvCheckPath = $@"%USERPROFILE%\Documents\1m-Grafy";
@@ -100,15 +133,75 @@ namespace _1m_CryptoGraphs_Downloader
             Console.ReadLine();
             //
 
-
-            if (Exchange == "BINANCE")
+            if (JsonAsk == "A")
             {
-                BINANCE(TradingPair, Exchange, StartT_miliseconds, StopT_miliseconds, SavePath);
-            }
+                int i = 0;
+                while (JSON_Config.Pairs.Count > i)
+                {
+                    TradingPair = JSON_Config.Pairs[i].TradingPair;
+                    Exchange = JSON_Config.Pairs[i].Exchange;
+                    StartDate = JSON_Config.Pairs[i].StartDate;
+                    StopDate = JSON_Config.Pairs[i].StopDate;
 
-            if (Exchange == "FTX")
+                    // Definuj časový rozsah, jak v sekundách, tak v milisekundách.
+                    TimeSpan StartT = DateTime.Parse(StartDate) - DateTime.UnixEpoch;
+                    int StartT_seconds = (int)StartT.TotalSeconds;
+                    double StartT_miliseconds = (double)StartT.TotalMilliseconds;
+                    TimeSpan StopT = DateTime.Parse(StopDate) - DateTime.UnixEpoch;
+                    int StopT_seconds = (int)StopT.TotalSeconds;
+                    double StopT_miliseconds = (double)StopT.TotalMilliseconds;
+                    //
+
+                    //
+                    string EnvPath = $@"%USERPROFILE%\Documents\1m-Grafy\{TradingPair}_{StartDate}_{StopDate}.csv";
+                    string SavePath = (Environment.ExpandEnvironmentVariables(EnvPath)).Replace("/", "");
+                    //
+
+                    // Info pro uživatele co vlastně aktuálně stahuje za pár, v případě výběru více párů. 
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Stahuji : ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{TradingPair}");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    //
+                    if (Exchange == "BINANCE")
+                        {
+                            BINANCE(TradingPair, Exchange, StartT_miliseconds, StopT_miliseconds, SavePath);
+                        }
+
+                    if (Exchange == "FTX")
+                    {
+                        FTX(TradingPair, Exchange, StartT_seconds, StopT_seconds, SavePath);
+                    }
+
+                    i++;
+                }
+
+            } else
             {
-                FTX(TradingPair, Exchange, StartT_seconds, StopT_seconds, SavePath);
+                // Definuj časový rozsah, jak v sekundách, tak v milisekundách.
+                TimeSpan StartT = DateTime.Parse(StartDate) - DateTime.UnixEpoch;
+                int StartT_seconds = (int)StartT.TotalSeconds;
+                double StartT_miliseconds = (double)StartT.TotalMilliseconds;
+                TimeSpan StopT = DateTime.Parse(StopDate) - DateTime.UnixEpoch;
+                int StopT_seconds = (int)StopT.TotalSeconds;
+                double StopT_miliseconds = (double)StopT.TotalMilliseconds;
+                //
+
+                //
+                string EnvPath = $@"%USERPROFILE%\Documents\1m-Grafy\{TradingPair}_{StartDate}_{StopDate}.csv";
+                string SavePath = (Environment.ExpandEnvironmentVariables(EnvPath)).Replace("/", "");
+                //
+
+                if (Exchange == "BINANCE")
+                {
+                    BINANCE(TradingPair, Exchange, StartT_miliseconds, StopT_miliseconds, SavePath);
+                }
+
+                if (Exchange == "FTX")
+                {
+                    FTX(TradingPair, Exchange, StartT_seconds, StopT_seconds, SavePath);
+                }
             }
 
             // Exekuce provedena, konečná.
@@ -320,6 +413,19 @@ namespace _1m_CryptoGraphs_Downloader
         {
             public bool success { get; set; }
             public List<FTX_result> result { get; set; }
+        }
+
+        class JSON_Config
+        {
+            public string TradingPair { get; set; }
+            public string StartDate { get; set; }
+            public string StopDate { get; set; }
+            public string Exchange { get; set; }
+        }
+
+        class JSON_Configs
+        {
+            public List<JSON_Config> Pairs{ get; set;}
         }
 
         static Stopwatch timer = new Stopwatch();
